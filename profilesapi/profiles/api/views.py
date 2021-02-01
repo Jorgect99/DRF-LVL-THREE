c3c2be6824b3ffbfe658b49ab94d78e64629788f
Jorgect99 <jorgect99@gmail.com>
2021-02-01 14:23:49 -0700

    Filtering

--- profilesapi/profiles/api/views.py
+++ profilesapi/profiles/api/views.py
@@ -1,11 +1,13 @@
 from rest_framework import generics
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import viewsets
 from rest_framework import mixins
+
 from profiles.models import Profile, ProfileStatus
 from profiles.api.serializers import ProfileSerializer, ProfileStatusSerializer, ProfileAvatarSerializer
-from profiles.api.permissions import IsOwnProfileReadOnly,IsOwnerOrReadOnly
+from profiles.api.permissions import IsOwnProfileReadOnly, IsOwnerOrReadOnly
 
 
 class AvatarUpdateView(generics.UpdateAPIView):
@@ -20,11 +22,20 @@ class ProfileViewSet(mixins.UpdateModelMixin, mixins.ListModelMixin, mixins.Retr
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated, IsOwnProfileReadOnly]
+    filter_backends = [SearchFilter]
+    search_fields = ["city"]
 
 class ProfileStatusViewSet(ModelViewSet):
-    queryset = ProfileStatus.objects.all()
     serializer_class = ProfileStatusSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        queryset = ProfileStatus.objects.all()
+        username = self.request.query_params.get('username', None)
+        if username is not None:
+            queryset = queryset.filter(user_profile__user__username=username)
+        return queryset
+
 
     def perform_create(self, serializer):
         user_profile = self.request.user.profile
