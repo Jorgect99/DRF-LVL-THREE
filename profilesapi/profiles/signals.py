3038fd575f0de483f56d0c41b0d95e9d386d9560
Jorgect99 <jorgect99@gmail.com>
2021-02-01 16:30:03 -0700

    Testing

--- profilesapi/profiles/signals.py
+++ profilesapi/profiles/signals.py
@@ -5,6 +5,6 @@ from profiles.models import Profile
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
-    print("created: ", created)
+    # print("created: ", created)
     if created:
         Profile.objects.create(user=instance)
